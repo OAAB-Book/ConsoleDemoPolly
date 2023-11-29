591854bc295fab8aa3e027ffd8d68ab8496ff0f6
Isak Pao <windperson@gmail.com>
2023-11-29 09:19:50 +0800

    Polly retry policy demo

---
+++ ConsoleDemoPolly/Program.cs
@@ -0,0 +1,29 @@
+﻿using Polly;
+
+var retryPolicy = Policy.Handle<MyException>().Retry(3);
+
+int executionCount = 0;
+
+retryPolicy.Execute(() =>
+{
+    Console.WriteLine("\r\n===\r\nTry Polly in command line");
+    if (executionCount < 3)
+    {
+        Console.WriteLine("Next I will throw the Exception");
+        executionCount++;
+        throw new MyException($"This is the {executionCount} time(s) exception");
+    }
+    Console.WriteLine("---");
+    Console.WriteLine("Oh Yes! We Passed!!!");
+});
+
+
+public class MyException : Exception
+{
+    public MyException(string message) : base(message)
+    {
+    }
+}
+
+
+
